81a9bfe32474df5530420ae62741f0e607a1c920
T300M256 <todd@todds-air.attlocal.net>
2015-01-28 18:24:57 -0500

    Repairs time after md5 is run
    Fixes #2. Unit test pass. Commented out start of code for handling
    arguments so this can run from main.

--- file_scan.py
+++ file_scan.py
@@ -10,18 +10,38 @@ Copyright 2015
 January 18, 2015        Created
 
 """
+#import argparse
 import datetime
 import os
-import glob
 import subprocess
-import time
 import re
+import calendar
+import sys
 
 HEADER = "#"*30+"\n" # header line
 DAYS_OLD = 6 * 30
-# note: we can use "find {{dir}}* -atime +30" to get files not accessed for 30 days
+
+#parser = argparse.ArgumentParser(description='Traverses given directory and reports on clear and potential redundant files and files that are not being used')
+#parser.add_argument('-d')
 
 iwd = os.getcwd()
+
+def file_date_to_spec(fdate):
+    """
+    takes a string that is a date string from a stat command run on a file and
+    returns a string that is a time specification that can be used by the touch
+    command to change the time metadata for a file e.g.,:
+    Jan 25 22:07:30 2015 = 201501252207.30
+    """
+    d = fdate.split(" ")
+    nd = ""
+    nd += d[3]
+    mnth = "{0:02d}".format(list(calendar.month_abbr).index(d[0]))
+    nd = nd+mnth+d[1]
+    tim = re.sub(":","",d[2],1)
+    tim = re.sub(":",".",tim,1)
+    nd += tim
+    return(nd)
 
 def find_identical_files(directory):
     """
@@ -31,36 +51,72 @@ def find_identical_files(directory):
     # go to the directory
     os.chdir(directory)
     
-    # Walk through directory
-    #for dName, sdName, fList in os.walk(inDIR):
-    #    for fileName in fList:
-    #        if fnmatch.fnmatch(fileName, pattern): # Match search string
-    #            fileList.append(os.path.join(dName, fileName))
+    # the problem wiht the md5 in our scan is that it causes the access time to be
+    # updated. This renders future scans of the directory when looking for old files
+    # to see them no older than the last scan. An approach to get around this would
+    # be to retrieve the access times for all the files using the stat command
+    # then use touch reset the access time to the original. This may change other
+    # time stats too need to look in that. Here is a command set example for
+    # changing the access times using touch:
+
+    # addressing access times
     
-    #find * -exec md5 '{}' \; # is the command that will essentially run md5 recursively
-    
-    # run the find command
+    # 1 - fetch all the previous accesstimes
     try:
-        find = subprocess.Popen("find * -exec md5 '{}' \;",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        find_stat = subprocess.Popen("find * -exec stat '{}' \;",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     except:
         raise
     # get the standard output
-    out, err = find.communicate() # get the standard output
-    md5_results = out.decode().split("\n") # split the text into a list
-    
-    file_by_md5 = {}
-    for f in md5_results:
-        if f == '':
+    out, err = find_stat.communicate() # get the standard output
+    fstats = out.decode().split("\n") # split the text into a list
+    fdates = {}
+    for s in fstats:
+        # parse stat output lines appear as follows:
+        #16777220 1001760 -rw-r--r-- 1 todd staff 0 7 "Jan 25 22:07:00 2015" "Jan 25 22:00:07 2015" "Jan 25 22:09:51 2015" "Jan 25 22:00:07 2015" 4096 8 0 bar.txt
+        if s == "":
             continue
-        p = re.split("\) = ",f)
+        at = re.search("\"[^\"]+\"",s).group(0)
+        at = at.strip('"')
+        dspec = file_date_to_spec(at)
+        ss = s.split(" ")
+        fn = " ".join(ss[27:])
+        fdates[fn] = dspec
+    
+
+    # get the md5 sums for each file...the side effect is the access time changes...but we repair these    
+    file_by_md5 = {}
+    for fn in fdates.keys():
+        
+        # run md5 sum and get the value in a dict
+        try:
+            cmd_md5 = subprocess.Popen("md5 "+fn,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        except:
+            raise
+        out, err = cmd_md5.communicate() # get the standard output
+        md5 = out.decode() # split the text into a list
+        md5 = md5.rstrip()
+        if md5 == '':
+            continue
+        p = re.split("\) = ",md5)
         if len(p) < 2:
             print("Failed to split "+f)
-        fn = re.sub("MD5 \(","",p[0])
+        fnn = re.sub("MD5 \(","",p[0])
+        if fnn != fn:
+            print("The file returned by md5 was not was not what was expected: "+fnn)
         if file_by_md5.__contains__(p[1]):
             file_by_md5[p[1]] += [ fn ]
         else:
             file_by_md5[p[1]] = [ fn ]
             
+        # repair access time using touch command e.g.:
+        # /usr/bin/touch -a -t 201501252207.30 bar.txt
+        tch = "/usr/bin/touch -a -t "+fdates[fn]+" "+fn
+        return_signal = subprocess.call(tch.split())
+        if return_signal != 0:
+            print("Could not run command "+tch)
+            sys.exit()
+  
+    # create our dict of list of files keyed by md5 sums
     identical = {}
     for md5 in file_by_md5.keys():
         if len(file_by_md5[md5]) == 1:
@@ -182,11 +238,6 @@ def get_report_string(directory):
     result = "##### file_scan.py Report\n"
     result += "##### "+datetime.datetime.now().ctime()+"\n"
     
-    # get list of old unused files - we have to do this first due to a flaw
-    # in that the find/md5 command used to find identical files appears to
-    # result the access time of files which old files uses
-    old = find_old_files(directory)
-    
     # get potential compressed and compressed files
     compressed = find_comp_uncomp(directory)
     
@@ -197,12 +248,18 @@ def get_report_string(directory):
     result += HEADER+"#### redundant content - identical md5sum\n"
     for md5 in identical.keys():
         mfiles = identical[md5]
+        mfiles.sort()
         result += "### "+md5+"\n"
         result += "\n".join(mfiles)+"\n"
    
     # prepare compressed/uncompressed report text
     result += HEADER+"#### potential compressed and uncompressed files\n"
     result += "\n".join(compressed)+"\n"
+
+    # get list of old unused files - we have to do this first due to a flaw
+    # in that the find/md5 command used to find identical files appears to
+    # result the access time of files which old files uses
+    old = find_old_files(directory)
    
     # prepare old file report text
     result += HEADER+"#### files not accessed in "+str(DAYS_OLD)+" days\n"
